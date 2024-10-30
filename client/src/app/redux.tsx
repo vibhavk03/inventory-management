3663868ae074b778b944032de9c011374b3326df
vibhavk03 <vibhavkashyap3@gmail.com>
2024-10-30 20:12:41 +0530

    disable eslint checks

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useRef } from "react";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
